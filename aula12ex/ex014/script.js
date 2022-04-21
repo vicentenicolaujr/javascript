5c87d5777bd0b95a07d0dfd8db4bda02e572fddb
Vicente de Brito Nicolau Junior <vicentejr1975@gmail.com>
2022-04-21 02:38:55 -0300

    Update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,5 +3,15 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora => 0 && hora < 12) {
+        img.src = 'dia.xcf'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tarde.xfc'
+        document.body.style.style.background = '#b9846f'
+    } else {
+        img.src = 'noite.xfc'
+        document.body.style.background = '#515154'
+    }
 }
