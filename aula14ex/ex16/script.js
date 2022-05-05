7f84245a85126d5fc90df6313d6f41f2c1763054
Vicente de Brito Nicolau Junior <vicentejr1975@gmail.com>
2022-05-04 23:26:05 -0300

    Update script.js

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -6,17 +6,22 @@ function contar() {
     var res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: '
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
-            for (var c = i; c <= f; c += p) {
+            for(var c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449} `
             }
         } else {
-            for (var c = i; c >= f; c += p) {
+            for(var c = i; c >= f; c -= p) {
                 res.innerHTML += `${c} \u{1F449} `
             }
         }
