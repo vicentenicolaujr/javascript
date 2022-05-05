01b7c5aff279f91f406c2a8ba5e304829a1fd489
Vicente de Brito Nicolau Junior <vicentejr1975@gmail.com>
2022-05-05 00:33:18 -0300

    Update

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -6,9 +6,11 @@ function tabuada() {
     } else {
         var n = Number(num.value)
         var c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             var item = document.createElement('option')
-            item.text = `${num} x ${c} = ${num*c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
