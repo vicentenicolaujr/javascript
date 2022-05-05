e2e52da699554dfa2834c677d379e8afafac5462
Vicente de Brito Nicolau Junior <vicentejr1975@gmail.com>
2022-05-05 00:06:49 -0300

    Update

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
